922aeb052be6d0fb698098ac80c681a4972b8861
Prajuval <guntiprajuval05@gmail.com>
2024-07-25 13:22:49 +0530

    commited files

---
+++ simple.py
@@ -0,0 +1,8 @@
+# Simple Python program
+
+# Input from user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Print a message using the input
+print(f"Hello, {name}! You are {age} years old.")
